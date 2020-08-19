5d15757a645523d7a9c4bd57f5f5f74d2c6e6248
Shirin-0918 <69805254+Shirin-0918@users.noreply.github.com>
2020-08-19 19:25:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+song="";
+function preload(){
+    song=loadSound("music.mp3")
+}
+function setup(){
+   canvas= createCanvas(600,500);
+   canvas.position(380,200)
+   video= createCapture(VIDEO);
+   video.hide();
+}
+function draw(){
+   image(video, 0,0,600,500)
+}
+function play(){
+    song.play();
+}
