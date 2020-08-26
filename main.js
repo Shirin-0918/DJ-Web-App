bef37adcf32eb39adfbdcb3e071d8b679eac675e
Shirin-0918 <69805254+Shirin-0918@users.noreply.github.com>
2020-08-26 20:09:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,12 +13,36 @@ function setup(){
    video= createCapture(VIDEO);
    video.hide();
    poseNet=ml5.poseNet(video, modelLoaded);
-   poseNet.on('poses', gotPoses);
+   poseNet.on('pose', gotPoses);
 }
 function draw(){
    image(video, 0,0,600,500);
    fill("#FF0000");
 stroke("#FF0000");
+circle(rightWristX, rightWristY, 20);
+if(scoreRightWrist>0.2){
+    
+if(rightWristY.length>0 && rightWristY<100){
+    song.rate(0.5);
+    document.getElementById("speed").innerHTML="Speed=0.5 x"
+}
+else if(rightWristY.length>100 && rightWristY<200){
+    song.rate(1);
+    document.getElementById("speed").innerHTML="Speed=1.0 x"
+}
+else if(rightWristY.length>200 && rightWristY<300){
+    song.rate(1.5);
+    document.getElementById("speed").innerHTML="Speed=1.5 x"
+}
+else if(rightWristY.length>300 && rightWristY<400){
+    song.rate(2);
+    document.getElementById("speed").innerHTML="Speed=2.0 x"
+}
+else if(rightWristY.length>400 && rightWristY<500){
+    song.rate(2.5);
+    document.getElementById("speed").innerHTML="Speed=2.5 x"
+}
+}
 if(scoreLeftWrist>0.2){
     circle(leftWristX,leftWristY,20);
 leftWristYNo=Number(leftWristY);
@@ -27,7 +51,6 @@ volume=removeDecimal/500;
 document.getElementById("volume").innerHTML="Volume-"+volume;
 song.setVolume(volume); 
 }
-
 }
 function play(){
     song.play();
@@ -41,6 +64,7 @@ function gotPoses(results){
     if(results.length>0){
         console.log(results);
         scoreLeftWrist=results[0].pose.keypoints[9].score;
+        scoreRightWrist=results[0].pose.keypoints[10].score;
         console.log("score left wrist"+scoreLeftWrist);
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
@@ -49,4 +73,5 @@ function gotPoses(results){
         console.log("left wrist x position="+leftWristX+"left wrist y position="+leftWristY);
         console.log("right wrist x position="+rightWristX+"right wrist y position="+rightWristY);
     }
+
 }
